bebd6c4156053e6206594a55f88d0fc9cfbc2a12
gbwillian <mktpapatunes@gmail.com>
2026-02-28 19:05:57 -0300

    Fix login redirect + auth flow

--- orbty-connect-flow/src/pages/profile/PublicProfileKeyed.tsx
+++ orbty-connect-flow/src/pages/profile/PublicProfileKeyed.tsx
@@ -1,7 +1,9 @@
 import { useParams } from "react-router-dom";
-import PublicProfile from "@/pages/PublicProfile";
+import PublicProfile from "@/pages/profile/PublicProfile";
 
 export default function PublicProfileKeyed() {
-  const { id } = useParams();
-  return <PublicProfile key={id} />;
+  const { id } = useParams<{ id: string }>();
+
+  // key força remount quando muda o :id
+  return <PublicProfile key={id || "public-profile"} />;
 }
